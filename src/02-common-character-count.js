9e5cd786cd39ff43d7952f7b22284cc97ac15759
Kanstantsin Sakavets <kanstantsin.sakavets@gmail.com>
2021-04-07 21:26:25 +0300

    feat: implement file-names and common-characters-count

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,16 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const minS = Math.min(s1.length, s2.length) === s1.length ? s1.split('') : s2.split('');
+  const maxS = Math.max(s1.length, s2.length) === s2.length ? s2.split('') : s1.split('');
+  for (let i = 0; i < minS.length; i++) {
+    if (maxS.includes(minS[i])) {
+      maxS[maxS.indexOf(minS[i])] = '';
+      minS[i] = '';
+    }
+  }
+  return minS.filter((el) => el === '').length;
 }
 
 module.exports = getCommonCharacterCount;
--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,16 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const renamedNames = [...names];
+  for (let i = 0; i < names.length; i++) {
+    let counter = 0;
+    for (let j = 0; j < names.length; j++) {
+      if (names[i] === renamedNames[j]) counter++;
+      if (counter > 1 && names[i] === renamedNames[j]) renamedNames[j] += `(${counter - 1})`;
+    }
+  }
+  return renamedNames;
 }
 
 module.exports = renameFiles;
