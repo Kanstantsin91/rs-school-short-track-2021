00d4aa7a9e55d0fdb561df22440150939b5ad13b
Kanstantsin Sakavets <kanstantsin.sakavets@gmail.com>
2021-04-10 12:15:10 +0300

    feat: implement find-index

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,21 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let first = 0;
+  let last = array.length;
+  let middle = Math.floor((first + last) / 2);
+
+  while (array[middle] !== value) {
+    if (array[middle] > value) {
+      last = middle;
+      middle = Math.floor((first + last) / 2);
+    } else {
+      first = middle;
+      middle = Math.floor((first + last) / 2);
+    }
+  }
+  return middle;
 }
 
 module.exports = findIndex;
