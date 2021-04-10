bb3572ebf002ee8440949b88b93c7bb094342a5a
Kanstantsin Sakavets <kanstantsin.sakavets@gmail.com>
2021-04-10 09:50:51 +0300

    feat: implement dns-stats and stack

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,18 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const DNSStat = {};
+
+  for (let i = 0; i < domains.length; i++) {
+    const address = domains[i].split('.').reverse();
+    let domain = '';
+    for (let j = 0; j < address.length; j++) {
+      domain += `.${address[j]}`;
+      DNSStat[domain] = typeof DNSStat[domain] !== 'undefined' ? DNSStat[domain] + 1 : 1;
+    }
+  }
+  return DNSStat;
 }
 
 module.exports = getDNSStats;
